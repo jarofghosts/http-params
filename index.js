29a256c633b8a48e8ef5f85523d1de3ca80748cb
jesse keane <me@jessekeane.me>
2013-10-08 21:20:39 -0700

    im a dummy

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-var url = require('url');
+var url = require('url')
 
-exports.parse = function (request, callback) {
-  var method = request.method.toLowerCase();
+exports.parse = parse
+
+function parse(request, callback) {
+  var method = request.method.toLowerCase()
 
   if (method != 'post' && method != 'put') {
-    return callback && callback(null, url.parse(request.url, true).query);
+    return callback && callback(null, url.parse(request.url, true).query)
   }
                 
-  var params = '';
-  request.resume();
+  var params = ''
+
   request.on('data', function (data) {
-    params += data;
+    params += data
     if (params.length > 1e6) {
-      request.connection.destroy();
-      callback && callback({ error: 'param size exceeded' });
+      request.connection.destroy()
+      callback && callback(new Error('param size exceeded'))
     }
-  });
+  })
   
   request.on('end', function () {
     try {
@@ -23,7 +25,9 @@ exports.parse = function (request, callback) {
     } catch (e) {
       return callback && callback(e);
     }
-    callback && callback(null, JSON.parse(params));
-  });
-};
+    callback && callback(null, result)
+  })
+
+  request.resume()
+}
 
